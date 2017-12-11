58111f049c56ccb6ec366734004012f92e55afb7
Zach Bauer <zbauer91@gmail.com>
2017-12-11 18:35:02 -0500

    I think I fleshed out the meat of the populateGame() function. I just need to ad some css trickery in to give these discs some discy-life

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 //function to create default number of discs and add click listeners
+// start game
+populateGame()
+
+
+var gameVars = {
+  discIsHovering: false,
+  towerOrigin: null,
+  towerDestination: null;
+}
 
 var discs = {
   maxDiscs: 15,
   minDiscs: 3,
   numDiscs: 5,
+  discHeight: 30,
   towerHeight: function() {
-    return 10 + 15 * numDiscs
+    return this.discHeight * (this.numDiscs + 2)
   },
   colors: [
     "seagreen",
@@ -26,13 +36,46 @@ var discs = {
   ]
 }
 
-function populateGame() {
-  $(".tower").css("height", `${discs.towerHeight()}px`)
-  for (let i = 0; i < numDiscs; i++) {}
+var towers = {
+  one: [],
+  two: [],
+  three: []
 }
+
 // first: need to populate grid with towers
 // towers height will be a function of the number of discs. If we say each disc is 15px tall, multiply that by numOfDiscs
+
+function populateGame() {
+  let multiplier = 1 / discs.numDiscs
+  let currentMultiplier = 0
+  $(".tower").css("height", `${discs.towerHeight()}px`)
+
+  for (let i = 0; i < discs.numDiscs; i++) {
+    //add discs and give them an attribute to show "weight"
+    $("#discContainerOne").append(
+      `<div class="disc" data-weight=${i + 1} data-tower="1"></div>`
+    )
+    towers.one.push(`${i + 1}`)
+    this.on(
+      "click",
+      discHover(this.attr("data-weight"), this.attr("data-tower"))
+    )
+  }
+}
+
+function resetGame() {}
+
+function generateDiscs() {}
+
+function discHover(weight, tower) {
+  //check if disc is top disc. return empty if not
+  gameVars.discIsHovering = true
+  //change css on selected div and make it hover
+
+}
 
 // second: need to populate tower 1 with 5 discs (default). for loop, evelis, etc.
 
 // third: need to interact with discs somehow
+
+// draw the rest of the owl
