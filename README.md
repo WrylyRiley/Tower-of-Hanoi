9d5e0f1d688593cc151025f2b52b6afe385a7aa4
Zach Bauer <zbauer91@gmail.com>
2017-12-11 09:55:04 -0500

    created an object to hold characteristics about the discs, and added some information to the readme about future drag and drop planning

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Tower-of-Hanoi
 
 GA Project 1 - Tower of Hanoi game in-browser
+
+## Draggable HTML Element Inspiration
+
+[Draggable HTML ELement in JS](https://www.w3schools.com/howto/howto_js_draggable.asp)
---
+++ script.js
@@ -0,0 +1,33 @@
+//function to create default number of discs and add click listeners
+
+var discs = {
+  maxDiscs: 15,
+  minDiscs: 3,
+  numDiscs: 5,
+  colors: [
+    "seagreen",
+    "darkcyan",
+    "peachpuff",
+    "chocolate",
+    "cornflowerblue",
+    "darkkhaki",
+    "darkslateblue",
+    "darkseagreen",
+    "green",
+    "goldenrod",
+    "indianred",
+    "lightslategrey",
+    "olive",
+    "royalblue",
+    "saddlebrown"
+  ]
+}
+
+function populateGame() {
+  for (let i = 0; i < numDiscs; i++) {}
+}
+// first: need to populate grid with towers
+
+// second: need to populate tower 1 with 5 discs (default). for loop, evlis, etc.
+
+// third: need to interact ith discs somehow
