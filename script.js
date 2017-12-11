4ebb730484baecd3ddb594dcf1ff9de344b160ba
Zach Bauer <zbauer91@gmail.com>
2017-12-11 11:26:25 -0500

    Added in towers, and now working on a separate div that will act as a disc holder

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var discs = {
   maxDiscs: 15,
   minDiscs: 3,
   numDiscs: 5,
+  towerHeight: function() {
+    return 10 + 15 * numDiscs
+  },
   colors: [
     "seagreen",
     "darkcyan",
@@ -24,10 +27,12 @@ var discs = {
 }
 
 function populateGame() {
+  $(".tower").css("height", `${discs.towerHeight()}px`)
   for (let i = 0; i < numDiscs; i++) {}
 }
 // first: need to populate grid with towers
+// towers height will be a function of the number of discs. If we say each disc is 15px tall, multiply that by numOfDiscs
 
-// second: need to populate tower 1 with 5 discs (default). for loop, evlis, etc.
+// second: need to populate tower 1 with 5 discs (default). for loop, evelis, etc.
 
-// third: need to interact ith discs somehow
+// third: need to interact with discs somehow
