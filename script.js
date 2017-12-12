b69769fe3d5cfa160207e37022caefc6b205d31d
Zach Bauer <zbauer91@gmail.com>
2017-12-12 18:42:18 -0500

    Did some stuff, added a few functions, still working through how I'm going to implement this beast

--- script.js
+++ script.js
@@ -4,13 +4,15 @@
 var gameVars = {
   diskIsHovering: false,
   towerOrigin: null,
-  towerDestination: null
+  towerDestination: null,
+  moveCounter: 0,
+  sameTower: false
 }
 
 var disks = {
   maxDisks: 15,
   minDisks: 3,
-  numDisks: 15,
+  numDisks: 5,
   diskHeight: 25,
   towerHeight: function() {
     return this.diskHeight * (this.numDisks + 2)
@@ -45,9 +47,9 @@ var towers = {
   twoEnd: 5,
   threeStart: 5,
   threeEnd: 6,
-  tower1: [],
-  tower2: [],
-  tower3: []
+  "tower-1": [],
+  "tower-2": [],
+  "tower-3": []
 }
 
 // first: need to populate grid with towers
@@ -64,21 +66,24 @@ function populateGame() {
   for (let i = disks.numDisks; i > 0; i--) {
     gridRow -= 1
     //add disks and give them an attribute to show "weight"
-    generateDisk(i + 1, i - 1, gridRow, gridColumn)
+    generateDisk(i, i - 1, gridRow, gridColumn)
   }
+  generateEventListeners()
 }
 
-function resetGame() {}
+function resetGame() {
+  gameVars.moveCounter = 0
+}
 
 function generateDisk(diskNum, colorNum, gridRow, gridColumn) {
   // Create new disk
   let currentDisk = $("<div></div>", {
     class: "disk",
-    "data-weight": `${diskNum}`,
-    "data-tower": "1"
+    id: `disk-${diskNum}`,
+    "data-towerNum": "Tower1"
   })
-  // Unshift disk number to array. Maybe I'll use this for tracking
-  towers.tower1.unshift(`${diskNum}`)
+  // push disk numbers to array.
+  towers["tower-1"].push(`${diskNum}`)
   // Chaining CSS in jQuery, use JSON object: https://stackoverflow.com/questions/5094788/jquery-chaining
 
   $(".game-container").append(currentDisk)
@@ -86,8 +91,8 @@ function generateDisk(diskNum, colorNum, gridRow, gridColumn) {
   currentDisk.css({
     //
     "background-color": disks.colors[colorNum],
-    height: "100%",
-    width: disks.widths[diskNum - 1] + "%",
+    height: "95%",
+    width: disks.widths[diskNum] + "%",
     "grid-row": `${gridRow} / span 1`,
     "grid-column": `${gridColumn} / span 1`,
     "justify-self": "center",
@@ -99,35 +104,69 @@ function generateDisk(diskNum, colorNum, gridRow, gridColumn) {
 }
 
 function generateWidths() {
-  diskIncrement = 5.625
+  diskIncrement = 5.66666666
   for (let i = 15; i > 0; i--) {
-    disks.widths[i] = 5.625 + diskIncrement * i
+    disks.widths[i] = diskIncrement + diskIncrement * i
   }
 }
 
-function diskHover(clickedTower, tower) {
+function diskHover(tower) {
+  let workingTower = towers[tower]
+  let discNum = workingTower.length - 1
+  //tower format: tower-#
+  // let towerNumber = tower.substr(tower.length-1, tower.length)
   if (towers[tower]) {
+    if (gameVars.diskIsHovering && disks.sameTower) {
+      sameTower = false
+      gameVars.diskIsHovering = false
+      $(`#disc-${discNum}`).css({
+        "margin-bottom": "0px"
+      })
+      return
+    }
+    //change css on selected div and make it hover
     gameVars.diskIsHovering = true
+    $(`#disc-${discNum}`).css({
+      "margin-bottom": "20px"
+    })
+  } else {
+    return
   }
+}
 
-  //change css on selected div and make it hover
+function placeDisc() {
+  gameVars.moveCounter += 1
+  if (gameVars.towerOrigin === gameVars.towerDestination) {
+  }
 }
 
 function generateEventListeners() {
   for (let i = 0; i < 3; i++) {
-    currentTower = $("<div></div>", { id: `tower${i}`, class: "towerListener" })
+    currentTower = $("<div></div>", {
+      id: `tower-${i + 1}`,
+      class: "towerListener"
+    })
 
-    currentTower.on("click", diskHover(this, $(this).attr("id")))
+    currentTower.on("click", function() {
+      if (disks.diskIsHovering) {
+        gameVars.towerDestination = $(this).attr("id")
+        placeDisc()
+      } else {
+        gameVars.towerOrigin = $(this).attr("id")
+        diskHover(gameVars.towerOrigin)
+      }
+    })
 
     $(".game-container").append(currentTower)
 
     currentTower.css({
-      height: "100%",
-      width: "100%",
-      "background-color": "rgbs(0, 0, 0, 0)",
       "grid-row": "5 / span 16",
       // Applies to columns 3, 4, and 5
       "grid-column": `${i + 3} / span 1`,
+      height: "100%",
+      width: "95%",
+      "justify-self": "center",
+      "background-color": "rgba(50, 50, 200, 0.2)",
       "z-index": "10"
     })
   }
@@ -138,3 +177,5 @@ function generateEventListeners() {
 // third: need to interact with disks somehow
 
 // draw the rest of the owl
+
+function winConditionCheck() {}
